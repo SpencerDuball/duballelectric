bd5ec73c1d50de6e3851ae2f1fa96905361cae78
Spencer Duball <spencerduball@gmail.com>
2021-08-17 17:01:23 -0500

    Added blank /commercial, /more, /residential. Added favicons and meta tags to head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,49 @@
+import Head from "next/head";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
+import { NotificationSystemProvider } from "context/notification-system";
+import { Header, Footer, NotificationSystem } from "lib/ui";
 import { theme } from "../theme/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Duball Electric Inc.</title>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <meta name="author" content="Duball Electric Inc." />
+        <meta
+          name="description"
+          content={
+            "Duball Electric is an electrical contractor providing residential " +
+            "and commercial electrial services for the Cedar Rapids and " +
+            "surrounding areas."
+          }
+        />
+        <meta property="og:image" content="/images/DuballElectric.png" />
+        <meta
+          property="og:description"
+          content={
+            "Duball Electric is an electrical contractor providing residential " +
+            "and commercial electrial services for the Cedar Rapids and " +
+            "surrounding areas."
+          }
+        />
+        <meta property="og:title" content="Duball Electric Inc." />
+        <meta name="twitter:title" content="Duball Electric Inc." />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <NotificationSystemProvider>
+          <NotificationSystem placement="top-end" />
+          <Box as="main" display="grid">
+            <Header position="fixed" zIndex="banner" />
+            <Component {...pageProps} />
+            <Footer />
+          </Box>
+        </NotificationSystemProvider>
+      </ChakraProvider>
+    </>
   );
 }
 export default MyApp;
---
+++ pages/residential.tsx
@@ -0,0 +1,5 @@
+import { Box } from "@chakra-ui/react";
+
+export default function Residential() {
+  return <Box as="div">Residential page</Box>;
+}
