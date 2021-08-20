80d1cb1aa8f79d5dcdded0914ae6fb6126478538
Spencer Duball <spencerduball@gmail.com>
2021-08-19 19:24:09 -0500

    Removed notificationsystem link in /

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,13 @@
 import { Box } from "@chakra-ui/react";
-import { Hero, AboutUs, ContactUs, NotificationSystem } from "lib/ui";
-import { NotificationSystemProvider } from "context/notification-system";
+import { Hero, AboutUs, ContactUs } from "lib/ui";
 
 export default function Home() {
   return (
-    <NotificationSystemProvider>
-      <NotificationSystem placement="top-end" />
+    <>
       <Hero />
       <AboutUs />
       <Box as="div" w="80%" h="0.125em" bg="gray.100" justifySelf="center" />
       <ContactUs />
-    </NotificationSystemProvider>
+    </>
   );
 }
