762ae46ba89660e4e058abd09770ca3813a6973c
Spencer Duball <spencerduball@gmail.com>
2021-08-20 09:41:03 -0500

    Updated favicon

--- lib/ui/index.ts
+++ lib/ui/index.ts
@@ -1,8 +1,8 @@
 import { Header, HeaderPropsI } from "./Header";
 import { Footer, FooterPropsI } from "./Footer";
-import { Hero, HeroPropsI } from "./Hero";
-import { AboutUs, AboutUsPropsI } from "./AboutUs";
-import { ContactUs, ContactUsPropsI } from "./ContactUs";
+import { Hero, HeroPropsI } from "./index/Hero";
+import { AboutUs, AboutUsPropsI } from "./index/AboutUs";
+import { ContactUs, ContactUsPropsI } from "./index/ContactUs";
 
 export { Header, Footer, Hero, AboutUs, ContactUs };
 
