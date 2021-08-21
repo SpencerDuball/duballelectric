54300ed1a84756bb8cba3e8ceb9f7e78b68c16e4
Spencer Duball <spencerduball@gmail.com>
2021-08-21 18:09:47 -0500

    Added /commercial & /more pages

--- pages/commercial.tsx
+++ pages/commercial.tsx
@@ -1,5 +1,24 @@
 import { Box } from "@chakra-ui/react";
+import {
+  ServicePageIntro,
+  ServicePageIntroTitle,
+  ServicePageIntroDescription,
+  ServicePageIntroIcon,
+} from "lib/ui";
+import { Building } from "lib/svg/unicons";
 
-export default function Residential() {
-  return <Box as="div">Commercial page</Box>;
+export default function Commercial() {
+  return (
+    <Box as="div" display="grid" gridAutoRows="max-content">
+      <ServicePageIntro>
+        <ServicePageIntroTitle>Commercial</ServicePageIntroTitle>
+        <ServicePageIntroDescription>
+          We have the experience and equipment to get your business projects
+          done efficiently. Plus 24/7 service to ensure your business gets the
+          help it needs.
+        </ServicePageIntroDescription>
+        <ServicePageIntroIcon icon={<Building />} />
+      </ServicePageIntro>
+    </Box>
+  );
 }
--- pages/more.tsx
+++ pages/more.tsx
@@ -1,5 +1,23 @@
 import { Box } from "@chakra-ui/react";
+import {
+  ServicePageIntro,
+  ServicePageIntroTitle,
+  ServicePageIntroDescription,
+  ServicePageIntroIcon,
+} from "lib/ui";
+import { Jackhammer } from "lib/svg/unicons";
 
-export default function Residential() {
-  return <Box as="div">More page</Box>;
+export default function Commercial() {
+  return (
+    <Box as="div" display="grid" gridAutoRows="max-content">
+      <ServicePageIntro>
+        <ServicePageIntroTitle>More</ServicePageIntroTitle>
+        <ServicePageIntroDescription>
+          Pools, hot tubs, parking lots, generators, underground, overhead and
+          more - if it’s electrical, we can help!
+        </ServicePageIntroDescription>
+        <ServicePageIntroIcon icon={<Jackhammer />} />
+      </ServicePageIntro>
+    </Box>
+  );
 }
